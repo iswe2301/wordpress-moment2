9f6126af10bf97064e6f6c31a4ffb7140e2124fa
Isa Westling <isawestling.blogg@hotmail.com>
2024-11-11 16:24:27 +0100

    Skapat undersida för bucketlist

---
+++ bucketlist.php
@@ -0,0 +1,8 @@
+<!-- Sätt titel för sidan samt inkludera header -->
+<?php $title = "Butcketlist"; ?>
+<?php include 'includes/header.php'; ?>
+
+            <h2>Bucketlist</h2>
+            <p>Lite text...</p>
+            
+<?php include 'includes/footer.php'; ?>
