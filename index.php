a46e5dfe2ce48dbee4e57d6da64fbd296c667f8d
Isa Westling <isawestling.blogg@hotmail.com>
2024-11-11 16:24:09 +0100

    Skapat undersida för start

---
+++ index.php
@@ -0,0 +1,8 @@
+<!-- Sätt titel för sidan samt inkludera header -->
+<?php $title = "Startsida"; ?>
+<?php include 'includes/header.php'; ?>
+
+            <h2>Startsida</h2>
+            <p>Lite text...</p>
+            
+<?php include 'includes/footer.php'; ?>
