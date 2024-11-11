72cdff5c5ab66ca2a88a4d1ceee999ee45f5c1ad
Isa Westling <isawestling.blogg@hotmail.com>
2024-11-11 15:42:10 +0100

    Skapat set-metoder för namn, beskrivning och prioritet. Lagt till validering av dessa i addPost-metoden.

--- includes/classes/Bucketlist.class.php
+++ includes/classes/Bucketlist.class.php
@@ -20,12 +20,53 @@ class Bucketlist {
     // Metod för att lägga till en ny post till bucketlistan (db-tabellen)
     public function addPost(string $name, string $description, int $priority) : bool {
 
+        if(!$this->setName($name)) return false; // Kontrollera att namnet är giltigt
+        if(!$this->setDescription($description)) return false; // Kontrollera att beskrivningen är giltig
+        if(!$this->setPriority($priority)) return false; // Kontrollera att prioritet är giltig
+
        // Använd prepared statements för att undvika SQL-injections
        $stmt = $this->db->prepare("INSERT INTO bucketlist (name, description, priority) VALUES (?, ?, ?)");
        $stmt->bind_param("ssi", $name, $description, $priority);
 
        // Returnera true om det lyckas, annars false
        return $stmt->execute(); 
+    }
+
+    // Set-metod för att sätta namn
+    public function setName(string $name) : bool {
+        // Kontrollera att namnet inte är tomt och inte längre än 64 tecken
+        if(!empty($name) && strlen($name) <= 64) {
+            $this->name = $name; // Sätt namnet
+            return true; // Returnera true om det lyckas
+        } else {
+            return false; // Returnera false om det misslyckas
+        }
+    }
+
+    // Set-metod för att sätta beskrivning
+    public function setDescription(string $description) : bool {
+        // Kontrollera att beskrivningen inte är tomt och inte längre än 255 tecken
+        if(!empty($description) && strlen($description) <= 255) {
+            $this->description = $description; // Sätt beskrivningen
+            return true; // Returnera true om det lyckas
+        } else {
+            return false; // Returnera false om det misslyckas
+        }
+    }
+
+    // Set-metod för att sätta prioritet
+    public function setPriority(int $priority) : bool {
+
+        // Validera att det är en siffra
+        $priority = intval($priority);
+
+        // Kontrollera att prioritet är mellan 1 och 5
+        if($priority >= 1 && $priority <= 5) {
+            $this->priority = $priority; // Sätt prioritet
+            return true; // Returnera true om det lyckas
+        } else {
+            return false; // Returnera false om det misslyckas
+        }
     }
 }
 
