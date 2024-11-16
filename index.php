a4ee0818b0bfb4c9b2d9cf1aa138c66e8383b74c
Isa Westling <isawestling.blogg@hotmail.com>
2024-11-16 15:49:39 +0100

    Lagt till info om uppgiften

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <!-- Sätt titel för sidan samt inkludera header -->
-<?php $title = "Startsida"; ?>
+<?php $title = "Hem"; ?>
 <?php include 'includes/header.php'; ?>
 
-            <h2>Startsida</h2>
-            <p>Lite text...</p>
+            <h2>Om uppgiften</h2>
+            <p>Detta projekt har utvecklats som en uppgift i kursen Webbutveckling för Wordpress (DT209G) i Webbutvecklingsprogrammet på Mittuniversitet.</p>
+            <p>Uppgiften gick ut på att skapa en dynamisk webbapplikation med hjälp av HTML, CSS samt objektorienterad PHP,
+                där man kan hantera en bucketlist. Funktionaliteten inkluderar att skapa, läsa, uppdatera och radera poster (CRUD-operationer) i listan.
+                Projektet använder en MySQL-databas för att lagra data och erbjuder en responsiv design med stöd för mobilanvändare.</p>
+
+            <h2>PHP-erfarenheter</h2>
+            <p>Jag har tidigare aldrig arbetat med PHP, så det var en rolig erfarenhet att få testa på.
+                Mitt första intryck har varit väldigt positivt, och jag tycker att det varit relativt lätt att förstå sig på och komma in i,
+                speciellt när man har tidigare erfarenhet av JS och C# som jag anser kan vara en längre startsträcka och generellt kännas mer krångligt.</p>
+            <p>Det jag uppskattar med PHP är att jag upplever filstrukturen som enkel att förstå sig på, och att det är smidigt att arbeta med databasen.
+                Det känns som att det går att få till mycket funtionalitet med relativt lite kod.
+                Jag tycker det blir enklare att utveckla när man kan särskilja olika delar i olika filer och inkludera "komponenter" där det behövs.
+                Jag har inte känt mig lika "förvirrad" i mitt eget projekt så att säga, utan det har varit enkelt att navigera bland filer.</p>
+            <p>Jag ser verkligen fram emot att få utveckla mer i PHP i kommande projekt och lära mig ännu mer om den funktionalitet som finns!</p>
             
 <?php include 'includes/footer.php'; ?>
